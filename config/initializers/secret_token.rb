82ca8a0a2a7ed5e705d8efa5ad70b32b921b4efc
Mike Mangino <mmangino@elevatedcode.com>
2014-03-31 19:34:02 -0400

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cincyslides::Application.config.secret_key_base = 'e5a529f10f53f8f6b79652cf977f0dff50b5424b851afbb0f8dccd93b029831100e869063cd7a3544615514aefc344a3bc65a06ba5a83ff4de6c9054402d0c1b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cincyslides::Application.config.session_store :cookie_store, key: '_cincyslides_session'
